92321724ae984b7c51a059ec7713b7c5410394fc
yeomjinseop <yeomjinseop@gmail.com>
2023-04-16 15:47:36 +0900

    add Header Component

--- admin/src/components/Header.tsx
+++ admin/src/components/Header.tsx
@@ -1,8 +1,13 @@
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <>
+      <div className='adminHeader'>
+        <button>카테고리 관리</button>
+        <button>여행 상품 관리</button>
+        <button>광고 관리</button>
+        <button>기타 관리</button>
+      </div>
+    </>
   );
 }
 export default Header;
