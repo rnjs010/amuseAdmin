a056a62dc108ac9835eeb74ff923a3f81f62687d
Jwon <check4545@kakao.com>
2023-05-01 23:59:19 +0900

    Add DatePicker

--- admin/src/pages/AdPages/Ad.tsx
+++ admin/src/pages/AdPages/Ad.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from '../../components/Ad/Ad.module.css'
 import {Link} from "react-router-dom";
 
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 const Ad = () => {
 	
-	return(
+	const todayDate = new Date();
+	const [startDate, setStartDate] = useState<Date>(todayDate);
+	const [endDate, setEndDate] = useState<Date>(todayDate);
+	
+	useEffect(() => {
+		if(startDate > endDate) {
+			window.confirm("Invalid");
+			setStartDate(todayDate);
+			setEndDate(todayDate);
+		}
+	}, [startDate, endDate])
+	
+	return (
 		<div className={styles.container}>
 			<text className={styles.title}>
 				광고 관리
@@ -13,39 +28,47 @@ const Ad = () => {
 			<div className={styles.body}>
 				<div className={styles.searchComponent}>
 					<div
-						style={{display:"flex", flexDirection: "row", justifyContent: "space-between"}}
+						style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}
 					>
 						<input className={styles.searchBar}/>
 						
 						<button className={styles.searchBtn}
-							onClick={() => console.log("123123")}
+								onClick={() => console.log("123123")}
 						>
 							검색
 						</button>
 					</div>
 					<div className={styles.adList}>
+						
+						// TODO
 						<div className={styles.dateLabel}>
 							시작일
 						</div>
-						<button className={styles.showDatePickerBtn}></button>
 						
+						<DatePicker
+							className={styles.showDatePickerBtn}
+							onChange={(e) => setStartDate(e || startDate)}
+						>
+							<div>
+								 {String(startDate)}
+							</div>
+						</DatePicker>
+						
+						// TODO
 						<div className={styles.dateLabel}>
-							종료일
+							종료일 {String(endDate)}
 						</div>
-						<button className={styles.showDatePickerBtn}></button>
+						<DatePicker
+							className={styles.showDatePickerBtn}
+							onChange={(e) => setEndDate(e || startDate)}
+						/>
+						
 					</div>
 				</div>
 				
-				<div
-					style={{position: "relative", top:20, left: "35%", flexDirection: "row", display: "flex"}}
-				>
-					<Link to={'/ad/edit'} className={styles.linkBtn}>
-						수정
-					</Link>
-					
-					<Link to={'/'} className={styles.linkBtn}>
-						신규등록
-					</Link>
+				<div style={{position: "relative", top: 20, left: "35%", flexDirection: "row", display: "flex"}}>
+					<Link to={'/ad/edit'} className={styles.linkBtn}> 수정 </Link>
+					<Link to={'/'} className={styles.linkBtn}> 신규등록 </Link>
 				</div>
 				
 				
@@ -80,9 +103,8 @@ const Ad = () => {
 					</div>
 				</div>
 			</div>
-			
 		</div>
-		)
+	)
 }
 
 export default Ad;
