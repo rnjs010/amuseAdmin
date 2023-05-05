0d2ef48af5f6d8fae805861afa829e1042ff7c5d
Jwon <check4545@kakao.com>
2023-05-05 22:19:46 +0900

    Add Navigate

--- admin/src/components/Table/Table.tsx
+++ admin/src/components/Table/Table.tsx
@@ -1,113 +1,122 @@
-import React, { useMemo } from 'react';
-import { COLUMNS } from './Columns'
-import { usePagination, useTable } from 'react-table'
+import React from 'react';
+import {usePagination, useTable} from 'react-table'
 import './table.module.css'
-
+import {useNavigate} from "react-router-dom";
 
 
 // TODO
 // @ts-ignore
-const Table = ({ data }) => {
-    const columns = useMemo(() => COLUMNS, [])
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        nextPage,
-        previousPage,
-        canNextPage,
-        canPreviousPage,
-        pageOptions,
-        gotoPage,
-        pageCount,
-        setPageSize,
-        state,
-        prepareRow,
-    } = useTable({
-            // @ts-ignore
-            columns,
-            data,
-        },
-        usePagination
-    );
-
-    const { pageIndex, pageSize } = state
-
-    // @ts-ignore
+const Table = ({root = "", columns, data}) => {
+	
+	const navigate = useNavigate();
+	
+	const onClickRow = (id: number) => {
+		navigate(`/${root}/${id}`);
+	};
+	
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		page,
+		nextPage,
+		previousPage,
+		canNextPage,
+		canPreviousPage,
+		pageOptions,
+		gotoPage,
+		pageCount,
+		setPageSize,
+		state,
+		prepareRow,
+	} = useTable({
+			columns,
+			data,
+		},
+		usePagination
+	);
+	
+	const {pageIndex, pageSize} = state
+	
+	
+	// @ts-ignore
 	return (
-        <div className="table">
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
-                                    {column.render('Header')}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-
-                <tbody {...getTableBodyProps()}>
-                    {page.map((row: any) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell: any) => {
-                                    return (
-                                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-
-            <div className="table-pagination" style={{margin: '5px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {'<<'}
-                </button>
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                    Previous
-                </button>
-                <span>
-                    <strong style={{display: 'block', width: '100px', textAlign: 'center'}}>
-                        {pageIndex + 1} / {pageOptions.length}
-                    </strong>
-                </span>
-                <span>
-                    Go to page: {' '}
-                    <input type="number" defaultValue={pageIndex + 1}
-                    onChange={(e) => {
-                        const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
-                        gotoPage(pageNumber)
-                    }}
-                    style={{width: '50px'}} />
-                </span>
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    Next
-                </button>
-                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                    {'>>'}
-                </button>
-            </div>
-            <div className="table-pagesize" style={{margin: '5px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
-                <select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
-                    {
-                        [10, 25, 50].map(pageSize => (
-                            <option key={pageSize} value={pageSize}>
-                                {pageSize}개 씩 보기
-                            </option>
-                        ))
-                    }
-                </select>
-            </div>
-        </div>
-    );
+		<div style={{marginLeft: '5%', width: '90%'}}>
+		<div className="table">
+			<table {...getTableProps()}>
+				<thead>
+				{headerGroups.map((headerGroup) => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map((column) => (
+							<th {...column.getHeaderProps()}>
+								{column.render('Header')}
+							</th>
+						))}
+					</tr>
+				))}
+				</thead>
+				
+				<tbody {...getTableBodyProps()}>
+				{page.map((row: any) => {
+					prepareRow(row)
+					return (
+						<tr {...row.getRowProps()} onClick={() => onClickRow(row.original.id)}>
+							{row.cells.map((cell: any) => {
+								return (
+									<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+								);
+							})}
+						</tr>
+					
+					);
+				})}
+				</tbody>
+			</table>
+			
+			<div className="table-pagination"
+				 style={{margin: '5px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+				<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+					{'<<'}
+				</button>
+				<button onClick={() => previousPage()} disabled={!canPreviousPage}>
+					Previous
+				</button>
+				<span>
+					<strong style={{display: 'block', width: '100px', textAlign: 'center'}}>
+				{pageIndex + 1} / {pageOptions.length}
+					</strong>
+					</span>
+				<span>
+					Go to page: {' '}
+					<input type="number" defaultValue={pageIndex + 1}
+						   onChange={(e) => {
+							   const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+							   gotoPage(pageNumber)
+						   }}
+						   style={{width: '50px'}}/>
+					</span>
+				<button onClick={() => nextPage()} disabled={!canNextPage}>
+					Next
+				</button>
+				<button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+					{'>>'}
+				</button>
+			</div>
+			<div className="table-pagesize"
+				 style={{margin: '5px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
+				<select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
+					{
+						[10, 25, 50].map(pageSize => (
+							<option key={pageSize} value={pageSize}>
+								{pageSize}개 씩 보기
+							</option>
+						))
+					}
+				</select>
+			</div>
+		</div>
+		</div>
+	);
 }
 
 export default Table;
