c839afdf37eac30930916196c47afb81252e7083
Jwon <check4545@kakao.com>
2023-05-05 22:21:05 +0900

    Add Table Columns

---
+++ admin/src/components/Table/CategoryTableColumns.tsx
@@ -0,0 +1,22 @@
+export const CategoryTableColumns = [
+    {
+        Header : 'id',
+        accessor : 'id'
+    },
+    {
+        Header : '카테고리',
+        accessor : 'category'
+    },
+    {
+        Header : '등록일',
+        accessor : 'startDate'
+    },
+    {
+        Header : '등록자',
+        accessor : 'createdCategoryDate'
+    },
+    {
+        Header : '등록 상품 수',
+        accessor : 'count'
+    }
+];
--- admin/src/components/Table/NoticeTableColumns.tsx
+++ admin/src/components/Table/NoticeTableColumns.tsx
@@ -1,27 +1,14 @@
-export const COLUMNS = [
+
+
+
+export const NoticeTableColumns = [
     {
         Header : 'id',
         accessor : 'id'
     },
     {
-        Header : '광고 제목',
+        Header : '제목',
         accessor : 'title'
-    },
-    {
-        Header : '시작일',
-        accessor : 'startDate'
-    },
-    {
-        Header : '종료일',
-        accessor : 'endDate'
-    },
-    {
-        Header : '광고 종류',
-        accessor : 'adType'
-    },
-    {
-        Header : '카테고리',
-        accessor : 'adCategory'
     },
     {
         Header : '등록일자',
