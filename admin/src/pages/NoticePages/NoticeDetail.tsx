a49271402a23662a53bf3671c717781a058b6631
Jwon <check4545@kakao.com>
2023-05-11 16:34:21 +0900

    Implement Notice Datail

--- admin/src/pages/NoticePages/NoticeDetail.tsx
+++ admin/src/pages/NoticePages/NoticeDetail.tsx
@@ -1,8 +1,14 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import {useParams} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
+import styles from '../../components/Notice/NoticeDetail.module.css'
 import ToastEditor from "../../components/ToastEditor";
 
+{/*<ToastEditor*/
+}
+
+{/*></ToastEditor>*/
+}
 
 const NoticeDetail = () => {
 	
@@ -14,50 +20,95 @@ const NoticeDetail = () => {
 		navigate(`/staff/${id}`);
 	};
 	
+	const [noticeContent, setNoticeContent] = useState<string>("");
+	
 	return (
-		<>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>id</div>
-				<div style={{width: 'auto'}}>{id}</div>
-			</p>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>제목</div>
-				<div style={{width: 'auto'}}>테스트 제목입니다</div>
-			</p>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>등록일자</div>
-				<div style={{width: 'auto'}}>2023-05-01</div>
-			</p>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>등록자</div>
-				<div style={{width: 'auto'}}>Admin1</div>
-				<button style={{width: 50, height: 20, border: "1px solid"}}
-						onClick={() => onClickRoute(1)}
-				>상세
-				</button>
-			</p>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>수정일자</div>
-				<div style={{width: 'auto'}}>2023-05-01</div>
-			</p>
-			
-			<p style={{display: "flex", flexDirection: 'row'}}>
-				<div style={{width: 100}}>수정자</div>
-				<div style={{width: 'auto'}}>Admin2</div>
-				<button style={{width: 50, height: 20, border: "1px solid"}}
-						onClick={() => onClickRoute(2)}>
-					상세
-				</button>
-			</p>
-			{/*<ToastEditor*/}
-			
-			{/*></ToastEditor>*/}
-		</>
+		<div className={styles.container}>
+			<div className={styles.body}>
+				<form>
+					<p className={styles.p}>
+						<div className={styles.pTitle}>
+							<strong>ID</strong>
+						</div>
+						
+						<div className={styles.pTitle}>
+							<strong>{id}</strong>
+						</div>
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong>제목</strong>
+						
+						</div>
+						<input className={styles.textInput}
+							   type="text"
+							   name="categoryName"
+							   placeholder="수정할 제목을 입력해주세요"
+						/>
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong> 등록일자 </strong>
+						</div>
+						2023-05-01
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong> 등록자 </strong>
+						</div>
+						Admin1
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong> 수정일자 </strong>
+						</div>
+						2023-05-01
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong> 수정자 </strong>
+						</div>
+						Admin2
+					</p>
+					
+					<p className={styles.p}>
+						<div
+							className={styles.pTitle}
+						>
+							<strong> 공지사항 내용 </strong>
+						</div>
+						
+						<div>
+							<ToastEditor setStateValue={setNoticeContent} value={noticeContent}/>
+						</div>
+					</p>
+					
+					<div
+						className={styles.p}
+					>
+						<button className={styles.button}>
+							수정하기
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	
 	
 	)
 	
