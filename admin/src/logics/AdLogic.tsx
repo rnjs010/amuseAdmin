90c8f6d889b33f6217e3ed7a9677388fcbb6f6c8
Jwon <check4545@kakao.com>
2023-05-11 16:34:44 +0900

    Fix Url

--- admin/src/logics/AdLogic.tsx
+++ admin/src/logics/AdLogic.tsx
@@ -5,7 +5,8 @@ export const AdApiLogic = {
 	
 	
 	getTestAdList : (async () => {
-			return await axios.get(`${process.env.REACT_APP_API_URL}/test/api/ad/getList`);
+			// return await axios.get(`${process.env.REACT_APP_API_URL}/test/api/ad/getList`);
+			return await axios.get(`https://ammuse.store/test/api/ad/getList`);
 		})
 		
 		
