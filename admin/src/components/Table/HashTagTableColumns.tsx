666092ebf93ceae5cd377358d5fe7d8f9e0b9ce1
Jwon <check4545@kakao.com>
2023-05-14 13:42:47 +0900

    Add Table Columns https://trello.com/c/h5yIib0Z/7-%ED%95%B4%EC%8B%9C%ED%83%9C%EA%B7%B8-%ED%85%9C%ED%94%8C%EB%A6%BF

---
+++ admin/src/components/Table/HashTagTableColumns.tsx
@@ -0,0 +1,26 @@
+export const HashTagTableColumns = [
+    {
+        Header : 'ID',
+        accessor : 'id'
+    },
+    {
+        Header : '해시태그',
+        accessor : 'hashTag'
+    },
+    {
+        Header : '등록일',
+        accessor : 'createAt'
+    },
+    {
+        Header : '등록자',
+        accessor : 'createdBy'
+    },
+    {
+        Header : '수정일자',
+        accessor : 'updatedAt'
+    },
+    {
+        Header : '수정자',
+        accessor : 'updatedBy'
+    }
+];
