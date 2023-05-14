fbd11af25526d8ffaf91de8924f1172dbf83bb5d
Jwon <check4545@kakao.com>
2023-05-14 13:43:16 +0900

    Implement HashTag Page https://trello.com/c/h5yIib0Z/7-%ED%95%B4%EC%8B%9C%ED%83%9C%EA%B7%B8-%ED%85%9C%ED%94%8C%EB%A6%BF

---
+++ admin/src/pages/HashTagPages/HashTag.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import styles from '../../components/HashTag/HashTag.module.css'
+import axios from "axios";
+import Table from "../../components/Table/Table";
+import {HashTagTableColumns} from "../../components/Table/HashTagTableColumns";
+
+const HashTag = () => {
+	
+	return (
+		<div className={styles.container}>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					alignItems: "center",
+					borderBottom: "1px solid #eb1749",
+					margin: "30px 50px 0px 50px",
+					paddingBottom: 10
+				}}
+			>
+				<h2> 해시태그 관리 </h2>
+				
+				<button
+					className={styles.button}
+					onClick={() => console.log("등록하기")}
+				>
+					등록하기
+				</button>
+			</div>
+			
+			<div style={{paddingTop: 30}}>
+				<Table route={"hashtag"} columns={HashTagTableColumns} data={[]}/>
+			</div>
+		
+		</div>
+	)
+	
+}
+
+export default HashTag
