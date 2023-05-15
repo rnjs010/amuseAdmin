4e76d53a38cab9a33ce76545b3f16b87fa4d5e20
Jwon <check4545@kakao.com>
2023-05-15 22:11:53 +0900

    Fix Header Route Path

--- admin/src/components/Header/Header.tsx
+++ admin/src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
         <p>admin123</p>
       </div>
       <div className={styles.category}>
-        <button onClick={() => navigate('/hashtag')}>해시태그 관리</button>
+        <button onClick={() => navigate('/category')}>카테고리 관리</button>
         <button onClick={() => navigate('/product/create')}>여행 상품 관리</button>
         <button onClick={() => navigate('/ad')}>광고 관리</button>
         <button>기타 관리</button>
