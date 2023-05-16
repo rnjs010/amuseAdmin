88ad720e92ec52582092e6c52855674fde5e0675
Jwon <check4545@kakao.com>
2023-05-16 10:49:58 +0900

    Add Multi Element Handle

--- admin/src/components/Table/AdTableColumns.tsx
+++ admin/src/components/Table/AdTableColumns.tsx
@@ -1,42 +1,44 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+
 export const AdTableColumns = [
     {
-        Header : 'ID',
-        accessor : 'id'
+        Header: 'ID',
+        accessor: 'id'
     },
     {
-        Header : '광고 제목',
-        accessor : 'title'
+        Header: '광고 제목',
+        accessor: 'title'
     },
     {
-        Header : '시작일',
-        accessor : 'startDate'
+        Header: '시작일',
+        accessor: 'startDate'
     },
     {
-        Header : '종료일',
-        accessor : 'endDate'
+        Header: '종료일',
+        accessor: 'endDate'
     },
     {
-        Header : '광고 종류',
-        accessor : 'adType'
+        Header: '카테고리',
+        accessor: "adCategory",
+        Cell: (props: any) => {
+            return props.value.join(", ")
+        },
     },
-    {
-        Header : '카테고리',
-        accessor : 'adCategory'
-    },
+    
     {
         Header : '등록일자',
-        accessor : 'createdAdDate'
+        accessor : 'createdAt'
     },
     {
         Header : '등록자',
-        accessor : 'createdAd'
+        accessor : 'createdBy'
     },
     {
         Header : '수정일자',
-        accessor : 'updatedAdDate'
+        accessor : 'updatedAt'
     },
     {
         Header : '수정자',
-        accessor : 'updatedAd'
+        accessor : 'updatedBy'
     }
 ];
