a6560e13661660f2355a1441631746d4125063b7
Jwon <check4545@kakao.com>
2023-05-23 10:46:54 +0900

    Add ProductTable

---
+++ admin/src/components/Table/ProductTable.tsx
@@ -0,0 +1,26 @@
+export const ProductTable = [
+    {
+        Header : 'ID',
+        accessor : 'id'
+    },
+    {
+        Header : '상품명',
+        accessor : 'product'
+    },
+    {
+        Header : '등록일자',
+        accessor : 'createdAt'
+    },
+    {
+        Header : '등록자',
+        accessor : 'createdBy'
+    },
+    // {
+    //     Header : '수정일자',
+    //     accessor : 'updatedAt'
+    // },
+    // {
+    //     Header : '수정자',
+    //     accessor : 'updatedBy'
+    // }
+];
