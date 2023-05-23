23ae7ba3821bee5caac9cf427b3d2477dc658ad5
Jwon <check4545@kakao.com>
2023-05-23 10:45:55 +0900

    Add ComponentListTableColumn

---
+++ admin/src/components/Table/ComponentListTableColumn.tsx
@@ -0,0 +1,27 @@
+// @ts-ignore
+export const ComponentListTableColumn = [
+	{
+		Header: 'ID',
+		accessor: 'id'
+	},
+	{
+		Header: '컴포넌트명',
+		accessor: 'component'
+	},
+	{
+		Header: '순서',
+		accessor: 'sequence'
+	},
+	{
+		Header: '타입',
+		accessor: 'type'
+	},
+	{
+		Header: '등록일',
+		accessor: 'createAt'
+	},
+	{
+		Header: '등록자',
+		accessor: 'createdBy'
+	},
+];
