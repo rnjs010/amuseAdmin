2d69c9d074302a5e50de03bcba6bdeb0ee0a5539
Jwon <check4545@kakao.com>
2023-05-25 13:59:54 +0900

    Implement MainPageComponentAdd

--- admin/src/pages/ComponentManagePages/MainPageComponentAdd.tsx
+++ admin/src/pages/ComponentManagePages/MainPageComponentAdd.tsx
@@ -1,20 +1,143 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 import styles from '../../components/ComponentManage/ComponentManage.module.css'
 
 import SelectableTable from "../../components/Table/SelectableTable";
-import Table from "../../components/Table/Table";
-import {ProductTable} from "../../components/Table/ProductTable";
+import {ProductTableColumns} from "../../components/Table/ProductTableColumns";
 import {Editor} from "@toast-ui/react-editor";
-import DatePicker from "react-datepicker";
+import {CategoryLogic} from "../../logics/CategoryLogic";
+import {ItemLogic} from "../../logics/ItemLogic";
+import {ComponentManageLogic} from "../../logics/ComponentManageLogic";
+
+type ProductItem = {
+	id: number
+	title: string
+	categoryNames: string[]
+	createdAt: string
+	createdBy: string
+	updatedAt: string
+	updatedBy: string
+}
 
 const MainPageComponentAdd = () => {
 	
-	const [componentType, setComponentType] = useState<string>("List");
+	const [category, setCategory] = useState<string[]>([]);
+	const [component, setComponent] = useState<string>("")
+	const [componentType, setComponentType] = useState<string>("리스트");
+	
+	const [productListArr, setProductListArr] = useState<any>([]);
+	
+	const [selected, setSelected] = useState<any[]>([]);
+	
+	const [bannerTitle, setBannerTitle] = useState<string>("");
+	const [parsedHTML, setParsedHTML] = useState<string>("");
+	const parsedHTMLRef = useRef<Editor>(null);
+	const [pcBannerFileName, setPcBannerFileName] = useState("");
+	const [pcBanner, setPcBanner] = useState("");
+	const pcBannerRef = useRef<HTMLInputElement | null>(null);
+	const [pcBannerUrl, setPcBannerUrl] = useState<string>("");
+	const [mobileBannerFileName, setMobileBannerFileName] = useState("");
+	const [mobileBanner, setMobileBanner] = useState("");
+	const mobileBannerRef = useRef<HTMLInputElement | null>(null);
+	const [mobileBannerUrl, setMobileBannerUrl] = useState<string>("");
+	const [pcBannerLink, setPcBannerLink] = useState("");
+	const [mobileBannerLink, setMobileBannerLink] = useState("");
+	
+	const saveImgFile = (ref: any, setBannerFileName: any, setBanner: any,) => {
+		try {
+			if (ref != null) {
+				// @ts-ignore
+				setBannerFileName(ref.current.files[0].name);
+				const file = ref.current.files[0];
+				const reader = new FileReader();
+				reader.readAsDataURL(file);
+				reader.onloadend = () => {
+					console.log(reader.result)
+					setBanner(reader.result);
+				}
+			}
+		} catch {
+		
+		}
+	};
 	
 	const radioComponentTypeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setComponentType(event.target.value);
 	};
+	
+	const componentSubmit = async () => {
+		
+		// console.log({
+		// 	title: bannerTitle,
+		// 	type: "배너",
+		// 	createBy: "daw916@naver.com",
+		// 	sequence: 3,
+		// 	pcBannerFileName: pcBannerFileName,
+		// 	pcBannerBase64: pcBanner,
+		// 	pcBannerLink: pcBannerLink,
+		// 	mobileBannerFileName: mobileBannerFileName,
+		// 	mobileBannerBase64: mobileBanner,
+		// 	mobileBannerLink: mobileBannerLink,
+		// 	content: parsedHTML,
+		// 	createdBy: "daw916@naver.com",
+		// 	itemCode: selected
+		// })
+		
+		
+		if (componentType == "리스트") {
+			const response = await ComponentManageLogic.postComponent({
+				title: component,
+				type: "리스트",
+				createBy: "daw916@naver.com",
+				sequence: 3,
+				itemCode: selected
+			})
+			console.log(response)
+			return;
+		}
+		
+		
+		if (componentType == "배너") {
+			const response = await ComponentManageLogic.postComponent({
+				title: component,
+				type: "배너",
+				createBy: "daw916@naver.com",
+				sequence: 3,
+				pcBannerFileName: pcBannerFileName,
+				pcBannerBase64: pcBanner,
+				pcBannerLink: pcBannerLink,
+				mobileBannerFileName: mobileBannerFileName,
+				mobileBannerBase64: mobileBanner,
+				mobileBannerLink: mobileBannerLink,
+				content: parsedHTML,
+			})
+			console.log(response)
+			return;
+		}
+	};
+	
+	useEffect(() => {
+		(async () => {
+			const response = await CategoryLogic.getCategoryArr();
+			setCategory(response.map((v: any) => (v.displayHashTag)));
+		})();
+		
+		(async () => {
+			const response = await ItemLogic.getProductItems({
+				"option": 1,
+				"page": 0,
+				"limit": 100,
+				"categoryNames": category
+			})
+			setProductListArr(response);
+		})();
+		
+		
+	}, [])
+	
+	useEffect(() => {
+	
+	}, [componentType]);
 	
 	return (
 		<div className={styles.container}>
@@ -31,6 +154,7 @@ const MainPageComponentAdd = () => {
 						   type="text"
 						   name="componentName"
 						   placeholder="컴포넌트 이름을 입력하세요"
+						   onChange={(e) => setComponent(e.target.value)}
 					/>
 				</p>
 				
@@ -48,11 +172,10 @@ const MainPageComponentAdd = () => {
 							style={{display: "flex", flexDirection: "row", alignItems: "center", marginRight: 10}}
 						>
 							<input type="radio"
-								   checked={componentType == "List"}
-								   value={"List"}
+								   checked={componentType == "리스트"}
+								   value={"리스트"}
 								   id={"List"}
 								   onChange={radioComponentTypeHandler}
-							
 							/>
 							<div> 리스트</div>
 						</div>
@@ -61,8 +184,8 @@ const MainPageComponentAdd = () => {
 							style={{display: "flex", flexDirection: "row", alignItems: "center", marginRight: 10}}
 						>
 							<input type="radio"
-								   checked={componentType == "Banner"}
-								   value={"Banner"}
+								   checked={componentType == "배너"}
+								   value={"배너"}
 								   id={"Banner"}
 								   onChange={radioComponentTypeHandler}/>
 							<div> 배너</div>
@@ -72,8 +195,8 @@ const MainPageComponentAdd = () => {
 							style={{display: "flex", flexDirection: "row", alignItems: "center", marginRight: 10}}
 						>
 							<input type="radio"
-								   checked={componentType == "Tile"}
-								   value={"Tile"}
+								   checked={componentType == "타일"}
+								   value={"타일"}
 								   id={"Tile"}
 								   onChange={radioComponentTypeHandler}/>
 							<div> 타일</div>
@@ -82,50 +205,27 @@ const MainPageComponentAdd = () => {
 				</p>
 				
 				{
-					(componentType == "List") ? (
+					(componentType == "리스트") ? (
 						<div>
 							<p className={styles.p}>
 								<div
 									className={styles.pTitle}
 								>
-									<strong>싱품목록</strong>
+									<strong>상품목록</strong>
 								</div>
 							
 							
 							</p>
 							<SelectableTable
-								route={""} columns={ProductTable} data={[
-								{
-									id: 1,
-									product: "[2박 3일] 김에 바다의 역사에 대해서 알아볼까요?",
-									createdAt: "2023-05-17T17:30:47.55265",
-									createdBy: "daw916@naver.com",
-								},
-								{
-									id: 2,
-									product: "[2박 3일] 낭만 가득한 여수 여행",
-									createdAt: "2023-05-17T17:30:47.55265",
-									createdBy: "daw916@naver.com",
-								},
-								{
-									id: 3,
-									product: "[3박 4일] 청춘 가득한 제주 여행",
-									createdAt: "2023-05-17T17:30:47.55265",
-									createdBy: "daw916@naver.com",
-								},
-								{
-									id: 4,
-									product: "[2박 3일] 낭만 가득한 여수 여행",
-									createdAt: "2023-05-17T17:30:47.55265",
-									createdBy: "daw916@naver.com",
-								}]}
+								route={""} columns={ProductTableColumns} data={productListArr}
+								setStateValue={setSelected} value={selected}
 							/>
 						</div>
 					) : ("")
 				}
 				
 				{
-					(componentType == "Banner") ? (
+					(componentType == "배너") ? (
 						<div>
 							<p className={styles.p}>
 								<div
@@ -138,6 +238,7 @@ const MainPageComponentAdd = () => {
 									   type="text"
 									   name="adName"
 									   placeholder="등록할 광고의 이름을 입력해주세요."
+									   onChange={(e) => setBannerTitle(e.target.value)}
 								/>
 							</p>
 							
@@ -147,7 +248,33 @@ const MainPageComponentAdd = () => {
 									type="file"
 									accept="image/*"
 									id="pcBanner"
+									onChange={() => saveImgFile(pcBannerRef, setPcBannerFileName, setPcBanner)}
+									ref={pcBannerRef}
 								/>
+							</p>
+							
+							<p className={styles.p}>
+								{
+									(pcBannerUrl) ? (
+										<img
+											src={pcBannerUrl}
+											width={200}
+											alt="pcBannerUrl"
+										/>
+									) : (
+										(
+											pcBanner
+										) ? (
+											<img
+												src={pcBanner}
+												width={200}
+												alt="pcBanner"
+											/>
+										) : (
+											""
+										)
+									)
+								}
 							</p>
 							
 							<p className={styles.p}>
@@ -161,6 +288,8 @@ const MainPageComponentAdd = () => {
 									   type="text"
 									   name="pcBannerLink"
 									   placeholder="PC 배너의 링크를 입력해주세요."
+									   onChange={e => setPcBannerLink(e.target.value)}
+									   value={pcBannerLink}
 								/>
 							</p>
 							
@@ -170,8 +299,34 @@ const MainPageComponentAdd = () => {
 									type="file"
 									accept="image/*"
 									id="mobileBanner"
+									onChange={() => saveImgFile(mobileBannerRef, setMobileBannerFileName, setMobileBanner)}
+									ref={mobileBannerRef}
 								/>
 							
+							</p>
+							
+							<p className={styles.p}>
+								{
+									(mobileBannerUrl) ? (
+										<img
+											src={mobileBannerUrl}
+											width={200}
+											alt="mobileBannerUrl"
+										/>
+									) : (
+										(
+											mobileBanner
+										) ? (
+											<img
+												src={mobileBanner}
+												width={200}
+												alt="mobileBanner"
+											/>
+										) : (
+											""
+										)
+									)
+								}
 							</p>
 							
 							<p className={styles.p}>
@@ -184,6 +339,8 @@ const MainPageComponentAdd = () => {
 								<input className={styles.textInput}
 									   type="text"
 									   name="mobileBannerLink"
+									   onChange={e => setMobileBannerLink(e.target.value)}
+									   value={mobileBannerLink}
 								/>
 							</p>
 							
@@ -244,7 +401,7 @@ const MainPageComponentAdd = () => {
 				}
 				
 				{
-					(componentType == "Tile") ? (
+					(componentType == "타일") ? (
 						<div>
 							타일 개수 3개로 가정한 것
 							n 개의 타일 개수 입력받아서 아래 형식 n개로 나타낼 예정
@@ -276,31 +433,8 @@ const MainPageComponentAdd = () => {
 									<strong>상품 목록</strong>
 								</p>
 								<SelectableTable
-									route={""} columns={ProductTable} data={[
-									{
-										id: 1,
-										product: "[2박 3일] 김에 바다의 역사에 대해서 알아볼까요?",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 2,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 3,
-										product: "[3박 4일] 청춘 가득한 제주 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 4,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									}]}
+									route={""} columns={ProductTableColumns} data={productListArr}
+									setStateValue={setSelected} value={selected}
 								/>
 							</p>
 							
@@ -331,31 +465,8 @@ const MainPageComponentAdd = () => {
 									<strong>상품 목록</strong>
 								</p>
 								<SelectableTable
-									route={""} columns={ProductTable} data={[
-									{
-										id: 1,
-										product: "[2박 3일] 김에 바다의 역사에 대해서 알아볼까요?",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 2,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 3,
-										product: "[3박 4일] 청춘 가득한 제주 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 4,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									}]}
+									route={""} columns={ProductTableColumns} data={productListArr}
+									setStateValue={setSelected} value={selected}
 								/>
 							</p>
 							
@@ -387,31 +498,8 @@ const MainPageComponentAdd = () => {
 								</p>
 								
 								<SelectableTable
-									route={""} columns={ProductTable} data={[
-									{
-										id: 1,
-										product: "[2박 3일] 김에 바다의 역사에 대해서 알아볼까요?",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 2,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 3,
-										product: "[3박 4일] 청춘 가득한 제주 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									},
-									{
-										id: 4,
-										product: "[2박 3일] 낭만 가득한 여수 여행",
-										createdAt: "2023-05-17T17:30:47.55265",
-										createdBy: "daw916@naver.com",
-									}]}
+									route={""} columns={ProductTableColumns} data={productListArr}
+									setStateValue={setSelected} value={selected}
 								/>
 							</p>
 						
@@ -424,6 +512,7 @@ const MainPageComponentAdd = () => {
 					style={{marginBottom: 10}}
 				>
 					<button className={styles.button}
+							onClick={componentSubmit}
 					>
 						등록하기
 					</button>
