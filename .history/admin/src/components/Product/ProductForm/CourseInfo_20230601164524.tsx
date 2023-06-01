f7d378fefb29c5ea0f596ff1ae2a92fff2c354b7
yeomjinseop <yeomjinseop@gmail.com>
2023-06-01 16:46:22 +0900

    arrange commit 2

---
+++ .history/admin/src/components/Product/ProductForm/CourseInfo_20230601164524.tsx
@@ -0,0 +1,76 @@
+import styles from './CourseInfo.module.css';
+import CourseModal from '../../Modal/CourseModal';
+import { useState } from 'react';
+import { IoMdRemoveCircle } from 'react-icons/io';
+
+interface ImageFile {
+  fileName: string,
+  base64Data: string
+}
+
+interface Course {
+  title: string;
+  timeCost: string;
+  content: string;
+  image: ImageFile;
+}
+
+interface MainInfoProps {
+  onAdd(course: Course): void,
+  onRemove(course: Course): void
+}
+
+
+function CourseInfo({onAdd, onRemove} : MainInfoProps) {
+  const [courseModalOpen, setCourseModalOpen] = useState<boolean>(false);
+  const [courseList, setCourseList] = useState<Course[]>([]);
+
+  const toggleCourseModal = () => {
+    setCourseModalOpen((prev) => !prev);
+  }
+  const handleCourseModal = (course:Course) => {
+    toggleCourseModal();
+    setCourseList((prev) => [...prev, course]);
+    onAdd(course);
+  }
+  const removeCourse = (selectedCourse:Course) => {
+    setCourseList((prevCourses) => prevCourses.filter((course) => course.title !== selectedCourse.title));
+    onRemove(selectedCourse);
+  }
+
+  const renderCourseList = () => {
+    return (
+      <ul>
+        {courseList.map((course) => {
+          return (
+            <li className={styles.courseBox} key={course.title}>
+              <button className={styles.removeBtn} onClick={() => removeCourse(course)}><IoMdRemoveCircle/></button>
+              <div className={styles.textInfo}>
+                <p>제목: {course.title}</p>
+                <p>소요시간: {course.timeCost}</p>
+                <p>설명: {course.content}</p>
+              </div>
+              <img className={styles.courseImg} src={course.image.base64Data} alt="Course" />
+            </li>
+          )
+        })}
+      </ul>
+    )
+  };
+
+  
+  return (
+  <div className={`${styles.container} ${styles.course}`}>
+      <div>
+        <span className={styles.title}>코스 관리</span>
+        <button className={styles.addBtn} onClick={toggleCourseModal}>추가하기</button>
+        {courseModalOpen && <CourseModal onSave={handleCourseModal} onToggle={toggleCourseModal}/>}
+      </div>
+      <div className={`${styles.status} ${styles.course}`}>
+        {renderCourseList()}
+      </div>
+  </div>
+  );
+}
+
+export default CourseInfo;
