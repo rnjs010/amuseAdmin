2b9910e9473734586bf66ee65bd99499007e09d8
Jwon <check4545@kakao.com>
2023-06-01 21:15:07 +0900

    Add DropDown, SearchTextInput

--- admin/src/pages/ComponentEditingPages/ListComponentRegister.tsx
+++ admin/src/pages/ComponentEditingPages/ListComponentRegister.tsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from '../../components/ComponentEditing/component.module.css'
 
+import DropDown from "../../components/DropDown";
 
 const ListComponentRegister = () => {
 	
+	const [searchOption, setSearchOption] = useState<string>("검색조건");
+	
+	useEffect(() => {
+		console.log(searchOption)
+	}, [searchOption])
+	
 	return (
 		<div className={styles.container}>
-			ListComponentRegister
+			<div>
+				<p className={styles.p}>
+					<div
+						className={styles.pTitle}
+					>
+						<strong>상품목록</strong>
+					</div>
+				</p>
+				
+				<div
+					style={{display: "flex", flexDirection: "row", marginBottom: 30}}
+				>
+					<DropDown
+						option={searchOption}
+						setOption={setSearchOption}
+					/>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+						}}
+					>
+						<input
+							className={styles.searchTextInput}
+							placeholder={"검색어를 입력하세요"}
+						/>
+					</form>
+					
+				</div>
+
+			</div>
 		</div>
+	
+	
 	)
 }
 
