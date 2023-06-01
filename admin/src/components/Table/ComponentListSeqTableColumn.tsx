7e406678e2f3f99962e29223da50199584d1fac8
Jwon <check4545@kakao.com>
2023-06-01 13:42:19 +0900

    Add ComponentListSeqTableColumn

---
+++ admin/src/components/Table/ComponentListSeqTableColumn.tsx
@@ -0,0 +1,15 @@
+// @ts-ignore
+export const ComponentListSeqTableColumn = [
+	{
+		Header: 'ID',
+		accessor: 'id'
+	},
+	{
+		Header: '컴포넌트명',
+		accessor: 'title'
+	},
+	{
+		Header: '순서',
+		accessor: 'sequence'
+	},
+];
