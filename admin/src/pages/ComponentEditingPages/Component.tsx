e8b2a3616050b69e5fc091fc08b1433b4ea007f6
Jwon <check4545@kakao.com>
2023-06-01 20:28:10 +0900

    Add Table

--- admin/src/pages/ComponentEditingPages/Component.tsx
+++ admin/src/pages/ComponentEditingPages/Component.tsx
@@ -1,10 +1,68 @@
 import React from "react";
+import Table from "../../components/Table/Table";
+import styles from '../../components/ComponentEditing/component.module.css'
+
+
+// To Extract
+const ComponentTableColumns = [
+	{
+		Header: 'ID',
+		accessor: 'id'
+	},
+	{
+		Header: '상품명',
+		accessor: 'title'
+	},
+	{
+		Header: '상품 코드',
+		accessor: 'code'
+	},
+	{
+		Header: '등록일',
+		accessor: 'createdAt'
+	},
+	{
+		Header: '등록자',
+		accessor: 'createdBy'
+	},
+	{
+		Header: '수정일',
+		accessor: 'updatedAdDate'
+	},
+	{
+		Header: '수정자',
+		accessor: 'updatedAd'
+	},
+];
 
 const Component = () => {
 	
-	return(
-		<div>
-			컴포넌트 생성 추가 삭제 관리
+	return (
+		<div className={styles.container}>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					alignItems: "center",
+					borderBottom: "1px solid #eb1749",
+					margin: "30px 50px 0px 50px",
+					paddingBottom: 10
+				}}
+			>
+				<h2> 컴포넌트 관리 </h2>
+				
+				<button className={styles.button}
+						onClick={() => console.log("추가하기")}
+				>
+					추가하기
+				</button>
+			</div>
+			
+			<div style={{paddingTop: 30}}>
+				<Table route={""} columns={ComponentTableColumns} data={[]}/>
+			</div>
+			
 		</div>
 	)
 }
