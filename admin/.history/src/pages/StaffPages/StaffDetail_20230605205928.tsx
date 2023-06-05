b44e2a892e880578c1028b464b6c08b02d15581a
yeomjinseop <yeomjinseop@gmail.com>
2023-06-05 21:07:38 +0900

    recover youngjin's code

---
+++ admin/.history/src/pages/StaffPages/StaffDetail_20230605205928.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import GuideModal from './GuideModal';
+
+const StaffDetail = () => {
+	
+
+	return(
+		<div style={{margin: '18px'}}>
+			<GuideModal />
+		</div>
+	)
+}
+
+export default StaffDetail
