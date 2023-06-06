318930b0f7975ac20a12927a553385b6cb832efb
Jwon <check4545@kakao.com>
2023-06-07 02:10:46 +0900

    Add getPageList

--- admin/src/logics/PageLogic.tsx
+++ admin/src/logics/PageLogic.tsx
@@ -7,5 +7,10 @@ export const PageLogic = {
 		return response.data.data;
 	}),
 	
+	getPageDetail: (async (id: any) => {
+		const response = await axios.get(`https://ammuse.store/test/api/page/${id}`);
+		return response.data.data;
+	}),
+	
 }
 
