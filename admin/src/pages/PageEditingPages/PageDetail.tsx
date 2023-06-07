85d645e612f303a9a87b75fea44c1618044d8c7c
Jwon <check4545@kakao.com>
2023-06-08 01:48:54 +0900

    Implement PageDetail Edit

--- admin/src/pages/PageEditingPages/PageDetail.tsx
+++ admin/src/pages/PageEditingPages/PageDetail.tsx
@@ -5,6 +5,8 @@ import styles from "../../components/PageEditing/page.module.css"
 import {useParams} from "react-router-dom";
 import {PageLogic} from "../../logics/PageLogic";
 import {ComponentLogic} from "../../logics/ComponentLogic";
+import Table from "../../components/Table/Table";
+import {PageTableColumns} from "../../components/Table/PageTableColumns";
 
 const PageDetail = () => {
 	
@@ -12,6 +14,7 @@ const PageDetail = () => {
 	
 	const [name, setName] = useState<string>("");
 	const [sequence, setSequence] = useState<number>(0);
+	const [isDisplay, setIsDisplay] = useState<string>("활성화");
 	const [categoryImage, setCategoryImage] = useState("");
 	const [categoryImageFileName, setCategoryImageFileName] = useState("");
 	
@@ -19,7 +22,64 @@ const PageDetail = () => {
 	const [mainDescription, setMainDescription] = useState<string>("")
 	const [subDescription, setSubDescription] = useState<string>("")
 	
-	const [pageComponent, setPageComponent] = useState<any>([]);
+	const [pageComponentListArr, setPageComponentListArr] = useState<any>([]);
+	const [componentListArr, setComponentListArr] = useState<any>([]);
+	
+	const [pageListArr, setPageListArr] = useState<any>([]);
+	
+	const radioComponentTypeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setIsDisplay(event.target.value);
+	};
+	
+	const validationCheck = () => {
+		
+		if (!name) {
+			window.confirm("페이지 명을 입력하세요");
+			return false;
+		}
+		
+		if (!categoryImageFileName || !categoryImage) {
+			window.confirm("이미지를 첨부하세요");
+			return false;
+		}
+		
+		if (isDisplay == "활성화" && !sequence) {
+			window.confirm("배치순서를 확인하세요");
+			return false;
+		}
+		
+		if (!mainDescription || !subDescription) {
+			window.confirm("설명 입력을 다시 확인해주세요");
+			return false;
+		}
+		
+		if (pageComponentListArr.length == 0) {
+			window.confirm("추가할 컴포넌트를 선택해주세요");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	const submitPage = async () => {
+		
+		if (!validationCheck()) return;
+		
+		const response = await PageLogic.editPage(id, {
+			name: name,
+			fileName: categoryImageFileName,
+			base64Data: categoryImage,
+			sequence: sequence,
+			disable: isDisplay == "활성화" ? false : true,
+			mainDescription: mainDescription,
+			subDescription: subDescription,
+			pageComponentInfos: pageComponentListArr.map((v: any) => ({componentId: v.id}))
+		}).then(() => {
+			window.confirm("등록되었습니다.");
+			window.history.back();
+		}).catch(e => window.confirm(e))
+		
+	}
 	
 	useEffect(() => {
 		(async () => {
@@ -29,22 +89,21 @@ const PageDetail = () => {
 			setCategoryImage(response.imgUrl);
 			setMainDescription(response.mainDescription);
 			setSubDescription(response.subDescription);
-			
-			const pageComponentInfos = response.pageComponentInfos;
-			console.log(pageComponentInfos)
-			
-			pageComponentInfos.map((v: any) => {
-				
-				(async () => {
-					const response = await ComponentLogic.getComponentDetail(v.id);
-					// setPageComponent([...pageComponent, response]);
-					console.log(response)
-				})()
-			});
-			
-			console.log(response)
-		})()
+			setPageComponentListArr(response.pageComponentInfos);
+		})();
+		
+		(async () => {
+			const response = await PageLogic.getPageListNotDisable();
+			setPageListArr(response);
+		})();
+		
+		(async () => {
+			const response = await ComponentLogic.getComponentList();
+			setComponentListArr(response)
+		})();
+		
 	}, [])
+	
 	
 	const saveImgFile = () => {
 		
@@ -61,6 +120,18 @@ const PageDetail = () => {
 		}
 	};
 	
+	const componentListHandler = (component: any) => {
+		if (!pageComponentListArr.some((v: any) => v.id == component.id)) {
+			setPageComponentListArr([...pageComponentListArr, component]);
+			return;
+		}
+		
+		setPageComponentListArr(pageComponentListArr.filter((v: any) => v.id !== component.id));
+		return
+	}
+	
+	
+	// @ts-ignore
 	return (
 		<div className={styles.container}>
 			<div className={styles.body}>
@@ -77,6 +148,19 @@ const PageDetail = () => {
 						   value={name}
 						   placeholder="페이지 이름을 입력하세요"
 						   onChange={(e) => setName(e.target.value)}
+					/>
+				</p>
+				
+				<p className={styles.p}>
+					
+					<div
+						className={styles.pTitle}
+					>
+						<strong>페이지 목록</strong>
+					</div>
+					<Table route={"page"}
+						   columns={PageTableColumns}
+						   data={pageListArr}
 					/>
 				</p>
 				
@@ -128,6 +212,44 @@ const PageDetail = () => {
 								-
 							</div>
 						</button>
+					</div>
+				</p>
+				
+				<p className={styles.p}>
+					<div
+						className={styles.pTitle}
+					>
+						<strong> 카테고리 활성화 </strong>
+					</div>
+					
+					<div
+						style={{display: "flex", flexDirection: "row"}}
+					>
+						<div
+							style={{display: "flex", flexDirection: "row", alignItems: "center", marginRight: 10}}
+						>
+							<input type="radio"
+								   checked={isDisplay == "활성화"}
+								   value={"활성화"}
+								   id={"active"}
+								   onChange={radioComponentTypeHandler}
+							/>
+							<div> 활성화</div>
+						</div>
+						
+						<div
+							style={{display: "flex", flexDirection: "row", alignItems: "center", marginRight: 10}}
+						>
+							<input type="radio"
+								   checked={isDisplay == "비활성화"}
+								   value={"비활성화"}
+								   id={"Inactive"}
+								   onChange={radioComponentTypeHandler}
+							/>
+							<div> 비활성화</div>
+						</div>
+					
+					
 					</div>
 				</p>
 				
@@ -196,9 +318,51 @@ const PageDetail = () => {
 					</div>
 				</p>
 				
-				{
-					JSON.stringify(pageComponent, null, 4)
-				}
+				<p className={styles.p}>
+					<div className={styles.pTitle}>
+						<strong>등록된 컴포넌트</strong>
+					</div>
+					
+					{
+						pageComponentListArr.map((v: any, i: any) => (
+							<div key={i} className={styles.componentListCell}>
+								<div style={{marginLeft: 10, width: 150}}> id: {v.id} </div>
+								<div style={{marginLeft: 10, width: 300}}> title: {v.title} </div>
+								<div style={{marginLeft: 10, width: 150}}> type: {v.type} </div>
+							</div>
+						))
+					}
+				</p>
+				
+				
+				<p className={styles.p}>
+					<div className={styles.pTitle}>
+						<strong>컴포넌트 목록</strong>
+					</div>
+					{
+						componentListArr.map((v: any, i: any) => (
+							<div key={i} className={styles.componentListCell}>
+								<input
+									type={"checkbox"}
+									onChange={(e) => componentListHandler(v)}
+									value={v.id}
+									checked={pageComponentListArr.some((value: any) => value.id == v.id)}
+								/>
+								<div style={{marginLeft: 10, width: 150}}> id: {v.id} </div>
+								<div style={{marginLeft: 10, width: 300}}> title: {v.title} </div>
+								<div style={{marginLeft: 10, width: 150}}> type: {v.type} </div>
+							</div>
+						))
+					}
+				
+				</p>
+				<div className={styles.p}>
+					<button className={styles.button}
+							onClick={submitPage}
+					>
+						수정하기
+					</button>
+				</div>
 			</div>
 		</div>
 	)
